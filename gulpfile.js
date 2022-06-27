bdf393adf4c81760430678a19286c75e68eecdf6
PIvSky <piotr.iwansky@gmail.com>
2022-06-27 19:12:44 +0200

    edited structure in gulpfile-->repeared compiling to css

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('*.scss')
+  return src('scss/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['*.scss'], buildStyles)
+  watch(['scss/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
